ba0037b01fd274139b49d055ee547d045ef67427
Ian Davies <idavies@leapingfrogs.com>
2024-08-04 17:44:53 +0100

    WIP

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::io;
 use std::io::{stdin, stdout};
 use std::string::ToString;
 use std::sync::mpsc;
-use std::sync::mpsc::{Receiver, Sender};
+use std::sync::mpsc::Sender;
 
 use chrono::prelude::*;
 use clap::Parser;
@@ -16,12 +16,6 @@ use rocket::figment::Figment;
 use rocket::State;
 use serde::{Deserialize, Serialize};
 
-const REQUIRED_SCOPES: [&str; 3] = [
-    "https://www.googleapis.com/auth/calendar.readonly",
-    "https://www.googleapis.com/auth/calendar.events.readonly",
-    "https://www.googleapis.com/auth/calendar.settings.readonly",
-];
-
 #[derive(Serialize, Deserialize, Default, Clone)]
 struct AccountConfig {
     name: String,
@@ -29,7 +23,7 @@ struct AccountConfig {
     default: bool,
 }
 
-#[derive(Serialize, Deserialize, Default, Clone)]
+#[derive(Serialize, Deserialize, Clone)]
 struct ToilConfig {
     #[serde(skip)]
     args: Args,
@@ -40,14 +34,27 @@ struct ToilConfig {
     // accounts: Vec<AccountConfig>,
 }
 
+impl Default for ToilConfig {
+    fn default() -> Self {
+        Self {
+            args: Args::default(),
+            client_id: None,
+            client_secret: None,
+            redirect_uri: Some("http://localhost:8000/callback".to_string()),
+            refresh_token: None,
+        }
+    }
+}
+
 impl ToilConfig {
     fn new(args: Args) -> Self
     {
-        let mut cfg: ToilConfig = confy::load("momentary_toil", args.config.as_deref()).unwrap();
-        cfg.args = args;
-
-
-        cfg
+        let binding = args.config.clone();
+        let config_name = binding.as_deref();
+        ToilConfig {
+            args,
+            ..confy::load("momentary_toil", config_name).expect("Valid configuration")
+        }
     }
 
     fn save(&self) {
@@ -103,33 +110,61 @@ struct Args {
 }
 
 #[tokio::main]
-// #[launch]
 async fn main() {
     let args = Args::parse();
-    let mut cfg = get_checked_configuration(args, &mut CmdLine::new(stdin().lock(), stdout()));
 
-    let start_week = start_of_week();
-    let end_week = end_of_week();
+    let time_util = TimeUtil::new();
+    let start = time_util.start_of_week();
+    let end = time_util.end_of_week();
 
-    println!(
-        "Week:: {:?} -> {:?}",
-        start_week.date_naive(),
-        end_week.date_naive()
+    let mut cfg = get_checked_configuration(
+        args,
+        Box::new(CmdLine::new(stdin().lock(), stdout())),
     );
 
-    let (tx, rx) = mpsc::channel();
-    let _join_handle = tokio::spawn(async move {
-        rocket::custom(Figment::from(rocket::Config::default()).join(("log_level", "off")))
-            .manage(MyState { tx })
-            .mount("/", routes![callback])
-            .launch()
-            .await
-    });
+    let events = block_on(retreive_events(&mut cfg, start, end));
+    let (summary, total_duration_minutes) = build_results(events, &cfg);
 
-    block_on(do_call(rx, &mut cfg, start_week, end_week));
+    let work_hours_per_week = 40.0;
+    let meeting_hours = total_duration_minutes as f32 / 60.0;
+    let non_meeting_hours = work_hours_per_week - meeting_hours;
+    let meeting_percentage = meeting_hours * 100.0 / work_hours_per_week;
+
+    let mut helper = CmdLine::new(stdin().lock(), stdout());
+
+    helper.output(format!("Week:: {:?} -> {:?}", start.date_naive(), end.date_naive()));
+
+    helper.output(format!("  Total meeting time this week: {meeting_hours:.2} / {work_hours_per_week:.2} [{meeting_percentage:.2}%]"));
+    helper.output(format!("  Non-Meeting time: {non_meeting_hours:.2}"));
+    for s in summary {
+        helper.output(format!("    > {}", s));
+    }
 }
 
-fn get_checked_configuration(args: Args, helper: &mut dyn CmdLineHelper) -> ToilConfig
+fn build_results(events: Vec<ToilEvent>, cfg: &ToilConfig) -> (Vec<String>, i64) {
+    let mut summary: Vec<String> = Vec::new();
+    let total_duration: i64 = events.iter().map(|e| {
+        let duration_minutes = e.end.signed_duration_since(e.start).num_minutes();
+        if e.start.hour() < 18 && e.end.hour() > 7
+        {
+            if cfg.args.details {
+                summary.push(format!(
+                    "{} {}: [{:.2} minutes] {}",
+                    e.start.weekday(),
+                    e.start.time(),
+                    duration_minutes,
+                    e.summary,
+                ));
+            }
+            duration_minutes
+        } else {
+            0
+        }
+    }).sum();
+    (summary, total_duration)
+}
+
+fn get_checked_configuration(args: Args, mut helper: Box<dyn CmdLineHelper>) -> ToilConfig
 {
     let mut cfg =
         if cfg!(test) {
@@ -156,22 +191,38 @@ fn get_checked_configuration(args: Args, helper: &mut dyn CmdLineHelper) -> Toil
     cfg
 }
 
-fn end_of_week() -> DateTime<Utc> {
-    day_of_week(Weekday::Sat)
+#[automock]
+trait DateTimeProvider {
+    fn now(self: &Self) -> DateTime<Utc> {
+        Utc::now()
+    }
 }
 
-fn start_of_week() -> DateTime<Utc> {
-    day_of_week(Weekday::Mon)
-}
+struct TimeUtil {}
 
-fn day_of_week(weekday: Weekday) -> DateTime<Utc> {
-    let current_week = Utc::now().iso_week();
-    let result = NaiveDate::from_isoywd_opt(current_week.year(), current_week.week(), weekday)
-        .and_then(|d| d.and_hms_opt(0, 0, 0))
-        .unwrap();
-    Utc.from_local_datetime(&result)
-        .single()
-        .expect("A valid date for the week")
+impl DateTimeProvider for TimeUtil {}
+
+impl TimeUtil {
+    fn new() -> Self {
+        Self {}
+    }
+    fn end_of_week(&self) -> DateTime<Utc> {
+        self.day_of_week(Weekday::Sat)
+    }
+
+    fn start_of_week(&self) -> DateTime<Utc> {
+        self.day_of_week(Weekday::Mon)
+    }
+
+    fn day_of_week(&self, weekday: Weekday) -> DateTime<Utc> {
+        let current_week = self.now().iso_week();
+        let result = NaiveDate::from_isoywd_opt(current_week.year(), current_week.week(), weekday)
+            .and_then(|d| d.and_hms_opt(0, 0, 0))
+            .unwrap();
+        Utc.from_local_datetime(&result)
+            .single()
+            .expect("A valid date for the week")
+    }
 }
 
 struct CmdLine<R, W>
@@ -185,9 +236,25 @@ where
     R: io::BufRead,
     W: io::Write,
 {
+    fn output(&mut self, msg: String) {
+        writeln!(&mut self.writer, "{}", msg).expect("should be able to write to stdout");
+    }
+}
+
+impl<R, W> CmdLine<R, W>
+where
+    R: io::BufRead,
+    W: io::Write,
+{
     fn new(reader: R, writer: W) -> Self {
         Self { reader, writer }
     }
+}
+
+struct ToilEvent {
+    start: DateTime<Utc>,
+    end: DateTime<Utc>,
+    summary: String,
 }
 
 #[automock]
@@ -216,12 +283,20 @@ where
     }
 }
 
-async fn do_call<'a>(
-    rx: Receiver<CallbackData>,
+async fn retreive_events<'a>(
     cfg: &mut ToilConfig,
     start: DateTime<Utc>,
     end: DateTime<Utc>,
-) {
+) -> Vec<ToilEvent> {
+    let (tx, rx) = mpsc::channel();
+    let _join_handle = tokio::spawn(async move {
+        rocket::custom(Figment::from(rocket::Config::default()).join(("log_level", "off")))
+            .manage(MyState { tx })
+            .mount("/", routes![callback])
+            .launch()
+            .await
+    });
+
     let access = "".to_string();
     let refresh = cfg.refresh_token.clone().unwrap_or("".to_string());
 
@@ -241,12 +316,11 @@ async fn do_call<'a>(
         // Get the URL to request consent from the user.
         // You can optionally pass in scopes. If none are provided, then the
         // resulting URL will not have any scopes.
-        let scopes: [String; 3] = REQUIRED_SCOPES
-            .into_iter()
-            .map(String::from)
-            .collect::<Vec<String>>()
-            .try_into()
-            .unwrap();
+        let scopes = [
+            "https://www.googleapis.com/auth/calendar.readonly".to_string(),
+            "https://www.googleapis.com/auth/calendar.events.readonly".to_string(),
+            "https://www.googleapis.com/auth/calendar.settings.readonly".to_string(),
+        ];
         let user_consent_url = gcal.user_consent_url(&scopes);
 
         // launch a server to receive the response...
@@ -277,87 +351,33 @@ async fn do_call<'a>(
 
     let e = Events::new(gcal);
     let events = e
-        .list_all(
-            "primary",
-            "",
-            0,
-            google_calendar::types::OrderBy::StartTime,
-            &[],
-            "",
-            &[],
-            false,
-            false,
-            true,
-            &end.to_rfc3339(),
-            &start.to_rfc3339(),
-            "",
-            "",
+        .list_all("primary", "", 0, google_calendar::types::OrderBy::StartTime,
+                  &[], "", &[], false, false,
+                  true, &end.to_rfc3339(), &start.to_rfc3339(), "", "",
         )
         .await
         .expect("A list of events");
 
-    let mut summary: Vec<String> = Vec::new();
-    let total_duration: i64 = events
+    events
         .body
         .iter()
         .filter(|e| {
             !e.event_type.eq("workingLocation")
                 && e.start.as_ref().map(|s| s.date.is_none()).unwrap_or(false)
                 && e.end.as_ref().map(|e| e.date.is_none()).unwrap_or(false)
+                && (e.attendees.iter().find(|a| a.self_ && a.response_status != "declined").is_some() || e.organizer.as_ref().map_or(false, |o| o.self_))
         })
-        .map(|e| {
-            if let (Some(start), Some(end)) = (
-                e.start.as_ref().and_then(|s| s.date_time),
-                e.end.as_ref().and_then(|e| e.date_time),
+        .filter_map(|e| {
+            match (
+                e.start.as_ref().and_then(|x| x.date_time),
+                e.end.as_ref().and_then(|x| x.date_time)
             ) {
-                let duration = end.signed_duration_since(start);
-                let attendee = e
-                    .attendees
-                    .iter()
-                    .find(|a| a.self_ && a.response_status != "declined");
-
-                let start_hour = e
-                    .start
-                    .as_ref()
-                    .and_then(|s| s.date_time.map(|d| d.hour()))
-                    .unwrap_or(0);
-                let end_hour = e
-                    .end
-                    .as_ref()
-                    .and_then(|s| s.date_time.map(|d| d.hour()))
-                    .unwrap_or(0);
-                if (start_hour < 18 && end_hour > 7)
-                    && (attendee.is_some() || e.organizer.as_ref().map_or(false, |o| o.self_))
-                {
-                    summary.push(format!(
-                        "{} {}: [{:.2} minutes] {}",
-                        start.weekday(),
-                        start.time(),
-                        duration.num_minutes(),
-                        e.summary,
-                    ));
-                    duration.num_minutes()
-                } else {
-                    0
-                }
-            } else {
-                0
+                (Some(start), Some(end)) => Some(
+                    ToilEvent { start, end, summary: e.summary.clone() }
+                ),
+                _ => None,
             }
-        })
-        .sum();
-
-    let work_hours_per_week = 40.0;
-    let meeting_hours = total_duration as f32 / 60.0;
-    let non_meeting_hours = work_hours_per_week - meeting_hours;
-    let meeting_percentage = meeting_hours * 100.0 / work_hours_per_week;
-
-    println!("  Total meeting time this week: {meeting_hours:.2} / {work_hours_per_week:.2} [{meeting_percentage:.2}%]");
-    println!("  Non-Meeting time: {non_meeting_hours:.2}");
-    if cfg.args.details {
-        for s in summary {
-            println!("    > {}", s);
-        }
-    }
+        }).collect()
 }
 
 #[cfg(test)]
@@ -365,11 +385,6 @@ mod tests {
     use mockall::predicate;
 
     use super::*;
-
-    #[test]
-    fn tests_available() {
-        assert!(true);
-    }
 
     #[test]
     fn test_get_checked_configuration() {
@@ -386,7 +401,7 @@ mod tests {
         mock.expect_get_user_input().with(predicate::eq("Enter your client_secret: ")).times(1).returning(|_| Ok("client_secret".to_string()));
         mock.expect_get_user_input().with(predicate::eq("Enter your redirect_uri: ")).times(1).returning(|_| Ok("redirect_uri".to_string()));
 
-        let config = get_checked_configuration(args, &mut mock);
+        let config = get_checked_configuration(args, Box::new(mock));
         assert_eq!(config.client_id, Some("client_id".to_string()));
         assert_eq!(config.client_secret, Some("client_secret".to_string()));
         assert_eq!(config.redirect_uri, Some("redirect_uri".to_string()));
