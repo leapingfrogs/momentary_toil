7ab2d93c69fae772694b6e3332ae3a211b5859de
Ian Davies <idavies@leapingfrogs.com>
2024-08-04 14:31:40 +0100

    Stake in the ground spec for some of the base configuration prompts.

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,56 @@
 #[macro_use]
 extern crate rocket;
 
-use chrono::prelude::*;
-use clap::Parser;
-use futures::executor::block_on;
-use google_calendar::{events::Events, Client};
-use rocket::figment::Figment;
-use rocket::State;
-use serde::{Deserialize, Serialize};
 use std::io;
+use std::io::{stdin, stdout};
 use std::string::ToString;
 use std::sync::mpsc;
 use std::sync::mpsc::{Receiver, Sender};
+
+use chrono::prelude::*;
+use clap::Parser;
+use futures::executor::block_on;
+use google_calendar::{Client, events::Events};
+use mockall::automock;
+use rocket::figment::Figment;
+use rocket::State;
+use serde::{Deserialize, Serialize};
 
 const REQUIRED_SCOPES: [&str; 3] = [
     "https://www.googleapis.com/auth/calendar.readonly",
     "https://www.googleapis.com/auth/calendar.events.readonly",
     "https://www.googleapis.com/auth/calendar.settings.readonly",
 ];
+
+#[derive(Serialize, Deserialize, Default, Clone)]
+struct AccountConfig {
+    name: String,
+    refresh_token: Option<String>,
+    default: bool,
+}
+
 #[derive(Serialize, Deserialize, Default, Clone)]
 struct ToilConfig {
     client_id: Option<String>,
     client_secret: Option<String>,
     redirect_uri: Option<String>,
     refresh_token: Option<String>,
+    // accounts: Vec<AccountConfig>,
 }
 
 impl ToilConfig {
-    fn new() -> Self {
-        let mut cfg: ToilConfig = confy::load("momentary_toil", None).unwrap();
+    fn new(args: &Args, cmd_line: &mut dyn CmdLineHelper) -> Self
+    {
+        let mut cfg: ToilConfig = confy::load(&args.config, None).unwrap();
 
         if cfg.client_id.is_none() {
-            cfg.client_id = Some(get_user_input(None, "Enter your client_id: "));
+            cfg.client_id = cmd_line.get_user_input("Enter your client_id: ").ok();
         }
         if cfg.client_secret.is_none() {
-            cfg.client_secret = Some(get_user_input(None, "Enter your client_secret: "));
+            cfg.client_secret = cmd_line.get_user_input("Enter your client_secret: ").ok();
         }
-        if cfg.redirect_uri.is_none() {
-            cfg.redirect_uri = Some(get_user_input(None, "Enter your redirect_uri: "));
+        if cfg.redirect_uri.is_none() || !cfg.redirect_uri.eq(&Some(args.callback_url.clone())) {
+            cfg.redirect_uri = cmd_line.get_user_input("Enter your redirect_uri: ").ok();
         }
         confy::store("momentary_toil", None, cfg.clone()).unwrap();
         cfg
@@ -70,30 +83,29 @@ fn callback(code: &str, state: &str, st: &State<MyState>) -> &'static str {
 #[command(version, about, long_about = None)]
 struct Args {
     #[arg(short, long, default_value_t = false)]
-    gui: bool,
-
-    #[arg(short, long, default_value_t = false)]
     details: bool,
 
     #[arg(short, long, default_value_t = 1)]
     weeks: u8,
+
+    #[arg(long, default_value = "http://localhost:8000/callback")]
+    callback_url: String,
+
+    #[arg(short, long, default_value = "default")]
+    account_name: String,
+
+    #[arg(short, long, default_value = "momentary_toil")]
+    config: String,
 }
 
 #[tokio::main]
 // #[launch]
 async fn main() {
     let args = Args::parse();
-
-    let (tx, rx) = mpsc::channel();
-
-    let mut cfg = ToilConfig::new();
+    let mut cfg = get_checked_configuration(&args, &mut CmdLine::new(stdin().lock(), stdout()));
 
     let start_week = start_of_week();
     let end_week = end_of_week();
-    // cfg.client_id.get_or_insert_with(|| prompt(None, "Enter your client_id: "));
-    // cfg.client_secret.get_or_insert_with(|| prompt(None, "Enter your client_secret: "));
-    // cfg.redirect_uri.get_or_insert_with(|| prompt(None, "Enter your redirect_uri: "));
-    // confy::store("momentary_toil", None, cfg.clone()).unwrap();
 
     println!(
         "Week:: {:?} -> {:?}",
@@ -101,6 +113,7 @@ async fn main() {
         end_week.date_naive()
     );
 
+    let (tx, rx) = mpsc::channel();
     let _join_handle = tokio::spawn(async move {
         rocket::custom(Figment::from(rocket::Config::default()).join(("log_level", "off")))
             .manage(MyState { tx })
@@ -110,7 +123,11 @@ async fn main() {
     });
 
     block_on(do_call(rx, &mut cfg, &args, start_week, end_week));
-    // let _ = join_handle.await;
+}
+
+fn get_checked_configuration(args: &Args, cmd_line: &mut dyn CmdLineHelper) -> ToilConfig
+{
+    ToilConfig::new(&args, cmd_line)
 }
 
 fn end_of_week() -> DateTime<Utc> {
@@ -131,22 +148,48 @@ fn day_of_week(weekday: Weekday) -> DateTime<Utc> {
         .expect("A valid date for the week")
 }
 
-/// A function to display messages and get user input.
-/// It accepts an optional instruction and a mandatory message.
-/// If an instruction is given, it's displayed before the message.
-/// After displaying the messages, it takes user input,
-/// trims it down and returns it.
-fn get_user_input(optional_instruction: Option<&str>, mandatory_message: &str) -> String {
-    if let Some(instruction) = optional_instruction {
-        println!("{}", instruction);
-    }
-    println!("{}", mandatory_message);
-    let mut input_buffer = String::new();
-    io::stdin()
-        .read_line(&mut input_buffer)
-        .expect("Failed to read from stdin");
-    input_buffer.trim().to_string()
+struct CmdLine<R, W>
+{
+    reader: R,
+    writer: W,
 }
+
+impl<R, W> CmdLine<R, W>
+where
+    R: io::BufRead,
+    W: io::Write,
+{
+    fn new(reader: R, writer: W) -> Self {
+        Self { reader, writer }
+    }
+}
+
+#[automock]
+trait CmdLineHelper {
+    fn get_user_input(&mut self, mandatory_message: &str) -> io::Result<String>;
+}
+impl<R, W> CmdLineHelper for CmdLine<R, W>
+where
+    R: io::BufRead,
+    W: io::Write,
+{
+    /// A function to display messages and get user input.
+    /// It accepts an optional instruction and a mandatory message.
+    /// If an instruction is given, it's displayed before the message.
+    /// After displaying the messages, it takes user input,
+    /// trims it down and returns it.
+    fn get_user_input(&mut self, mandatory_message: &str) -> io::Result<String>
+    where
+        R: io::BufRead,
+        W: io::Write,
+    {
+        writeln!(&mut self.writer, "{}", mandatory_message)?;
+        let mut input_buffer = String::new();
+        self.reader.read_line(&mut input_buffer)?;
+        Ok(input_buffer.trim().to_string())
+    }
+}
+
 async fn do_call<'a>(
     rx: Receiver<CallbackData>,
     cfg: &mut ToilConfig,
@@ -262,7 +305,7 @@ async fn do_call<'a>(
                     && (attendee.is_some() || e.organizer.as_ref().map_or(false, |o| o.self_))
                 {
                     summary.push(format!(
-                        "{} {}: [{:.2}mins] {}",
+                        "{} {}: [{:.2} minutes] {}",
                         start.weekday(),
                         start.time(),
                         duration.num_minutes(),
@@ -294,10 +337,32 @@ async fn do_call<'a>(
 
 #[cfg(test)]
 mod tests {
+    use mockall::predicate;
+
     use super::*;
 
     #[test]
     fn tests_available() {
         assert!(true);
+    }
+
+    #[test]
+    fn test_get_checked_configuration() {
+        let args = Args {
+            details: false,
+            weeks: 1,
+            callback_url: "http://localhost:8000/callback".to_string(),
+            account_name: "default".to_string(),
+            config: "test".to_string(),
+        };
+        let mut mock = MockCmdLineHelper::new();
+        mock.expect_get_user_input().with(predicate::eq("Enter your client_id: ")).times(1).returning(|_| Ok("client_id".to_string()));
+        mock.expect_get_user_input().with(predicate::eq("Enter your client_secret: ")).times(1).returning(|_| Ok("client_secret".to_string()));
+        mock.expect_get_user_input().with(predicate::eq("Enter your redirect_uri: ")).times(1).returning(|_| Ok("redirect_uri".to_string()));
+
+        let config = get_checked_configuration(&args, &mut mock);
+        assert_eq!(config.client_id, Some("client_id".to_string()));
+        assert_eq!(config.client_secret, Some("client_secret".to_string()));
+        assert_eq!(config.redirect_uri, Some("redirect_uri".to_string()));
     }
 }
